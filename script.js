f513373c650f00896a8020570545b0eada0173ba
ashfakb <112721378+ashfakb@users.noreply.github.com>
2023-07-29 13:31:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const preloader = document.getElementById("preloader");
+const body = document.querySelector("body")
+
+window.addEventListener("load", () => {
+
+    preloader.style.opacity = "0"
+    body.style.overflowY = "scroll"
+    
+
+})
